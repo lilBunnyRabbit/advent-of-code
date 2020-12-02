6f7f2b840e767c61cec22bc716d568600ec87c89
lilBunnyRabbit <andraz_ms@hotmail.com>
2020-12-02 12:12:41 -0800

    Day 2

--- 2020/2020.ts
+++ 2020/2020.ts
@@ -1,10 +1,12 @@
 import { day_1 } from './day_1/day_1';
+import { day_2 } from './day_2/day_2';
 
-export const year2020 = () => executeDay();
+export const year2020 = () => executeDay(2);
 
 function executeDay(day?: number) {
     const days: { [key: number]: Function } = {
-        1: day_1
+        1: day_1,
+        2: day_2
     }
 
     if(day) {
