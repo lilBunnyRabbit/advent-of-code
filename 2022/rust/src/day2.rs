dc147eff5e379263705cff644d53eee8a2f7f758
lilBunnyRabbit <andraz_ms@hotmail.com>
2022-12-06 23:04:03 +0100

    [2022] [rust] Day 2

--- 2022/rust/src/day2.rs
+++ 2022/rust/src/day2.rs
@@ -2,9 +2,9 @@
 use crate::utils;
 
 pub fn day_2(data: String) {
-  println!("Day 1:");
+  println!("Day 2:");
   println!("- Part 1: {}", part_1(&data));
-  // println!("- Part 2: {}", day_1_part_2(&data));
+  println!("- Part 2: {}", part_2(&data));
 }
 
 static RPS: [[u32; 3]; 3] = [
@@ -35,6 +35,32 @@ fn part_1(data: &String) -> u32 {
     .map(|values| {
       let [f, s] = values_to_index(values);
       return RPS[f as usize][s as usize] + s + 1;
+    })
+    .sum::<u32>();
+}
+
+fn part_2(data: &String) -> u32 {
+  return data
+    .split("\n")
+    .map(|values| {
+      let [f, s] = values_to_index(values);
+      if s == 0 {
+        let index = RPS[f as usize].iter()
+          .position(|&x| x == 0)
+          .unwrap();
+
+        return (index as u32) + 1;
+      }
+
+      if s == 2 {
+        let index = RPS[f as usize].iter()
+          .position(|&x| x == 6)
+          .unwrap();
+
+        return (index as u32) + 6 + 1;
+      }
+
+      return 3 + f + 1;
     })
     .sum::<u32>();
 }
