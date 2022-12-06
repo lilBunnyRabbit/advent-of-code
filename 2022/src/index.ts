a4e4faeab746fbe6aeae31e11cc9c312f3eba33c
lilBunnyRabbit <andraz_ms@hotmail.com>
2022-12-06 22:05:10 +0100

    [2022] Day 5

--- 2022/src/index.ts
+++ 2022/src/index.ts
@@ -2,8 +2,10 @@ import Day1 from "./days/day-1/day1";
 import Day2 from "./days/day-2/day2";
 import Day3 from "./days/day-3/day3";
 import Day4 from "./days/day-4/day4";
+import Day5 from "./days/day-5/day5";
 
 // Day1.execute();
 // Day2.execute();
 // Day3.execute();
-Day4.execute();
+// Day4.execute();
+Day5.execute()
